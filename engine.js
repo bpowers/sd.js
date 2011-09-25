9189ede06b722eac1865a920adb163972e632cd6
Bobby Powers <bobbypowers@gmail.com>
2011-09-25 00:26:12 -0400

    format & move scanner to bottom

--- engine.js
+++ engine.js
@@ -1,6 +1,9 @@
 // Copyright 2011 Bobby Powers. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
+
+// if we're running under node this will be defined.  Otherwise, just
+// use a simple object.
 if (typeof exports === 'undefined')
     exports = {};
 boosd = exports;
@@ -25,23 +28,23 @@ const parseControlInfo = function(simspecs) {
     // into the ctrl object
     var ctrl = {};
     simspecs.children().map(function() {
-	var name = $(this).get(0).tagName.toLowerCase();
-	var val = parseFloat($(this).text());
-	ctrl[name] = val;
+        var name = $(this).get(0).tagName.toLowerCase();
+        var val = parseFloat($(this).text());
+        ctrl[name] = val;
     });
 
     // now validate that all of the requiredSpecs are there & were
     // converted to numbers correctly
     err = requiredSpecs.reduce(function(acc, e) {
-	if (!ctrl.hasOwnProperty(e) || isNaN(ctrl[e])) {
-	    console.log('simspecs missing ' + e);
-	    return true;
-	}
-	return acc;
+        if (!ctrl.hasOwnProperty(e) || isNaN(ctrl[e])) {
+            console.log('simspecs missing ' + e);
+            return true;
+        }
+        return acc;
     }, false);
     if (err) {
-	exports.err = ERR_BAD_TIME;
-	return null;
+        exports.err = ERR_BAD_TIME;
+        return null;
     }
 
     // finally pull out some of the supplimental info, like
@@ -59,10 +62,69 @@ var Macro = {};
 
 builtins = {
     'max': function(a, b) {
-	return a > b ? a : b;
+        return a > b ? a : b;
     }
 };
 
+/**
+   Extracts all <macro>'s into a usable format.
+
+   FIXME: I'm skipping macros for now, will come back to them after
+   the rest of this stuff works.
+
+   @param macros JQuery list of all the DOM's macros.
+   @return A validated map of all the defined macros.
+*/
+parseMacros = function(macros) {
+    if (macros.length === 0)
+        return {}
+
+    // FIXME: is it parm or param?
+    paramSet = {}
+    params = macros.children('parm').map(function() {
+        const name = $(this).text();
+        paramSet[name] = true;
+        return name;
+    });
+
+
+    console.log('vars: (' + params[0] + ' ' + params[1]);
+
+    return {};
+}
+
+/**
+   Converts a string of xml data represnting an xmile model into a
+   javascript object.  The resulting javascript object can give
+   information about the model (what variables does it have, what are
+   their equations, etc), and can also create simulation objects.
+   Simulation objects can be parametarized and run (simulated),
+   generating time-series data on all of the variables.
+*/
+exports.modelGen = function(xmlString) {
+    exports.err = null;
+
+    const model = {};
+    const xmile = $(xmlString);
+    if (!xmile || xmile.find('header smile').attr('version') != "1.0") {
+        exports.err = ERR_VERSION;
+        return null;
+    }
+    model.name = xmile.find('header name').text() || 'boosd model';
+
+    // get our time info: start-time, end-time, dt, etc.
+    model.ctrl = parseControlInfo(xmile.children('simspecs'));
+    if (!model.ctrl)
+        return null;
+
+    model.macros = parseMacros(xmile.children('macro'));
+    if (!model.macros)
+        return null;
+
+    return {};
+}
+
+//=============================================================== scanner ==//
 const reservedWords = {
     'if': true,
     'then': true,
@@ -107,30 +169,30 @@ Scanner.NUMBER = 'number';
 
 Scanner.prototype._getChar = function() {
     if (this._pos < this._len) {
-	this._pos += 1;
-	this._peek = this.text[this._pos];
+        this._pos += 1;
+        this._peek = this.text[this._pos];
     } else {
-	this._peek = null;
+        this._peek = null;
     }
 
     return this._peek;
 }
 Scanner.prototype._skipWhitespace = function() {
     do {
-	if (this._peek === '\n') {
-	    this._line += 1;
-	    this._lineStart = this._pos + 1;
-	}
-	if (!isWhitespace(this._peek))
-	    break;
+        if (this._peek === '\n') {
+            this._line += 1;
+            this._lineStart = this._pos + 1;
+        }
+        if (!isWhitespace(this._peek))
+            break;
     } while (this._getChar() !== null);
 }
 Scanner.prototype._fastForward = function(num) {
     this._pos += num;
     if (this._pos < this._len) {
-	this._peek = this.text[this._pos];
+        this._peek = this.text[this._pos];
     } else {
-	this._peek = null;
+        this._peek = null;
     }
 }
 Scanner.prototype._lexIdentifier = function(startPos) {
@@ -138,7 +200,7 @@ Scanner.prototype._lexIdentifier = function(startPos) {
     const len = ident.length;
     this._fastForward(len);
     return new Token(ident, Scanner.IDENT, startPos,
-		     new SourceLoc(startPos.line, startPos.pos + len));
+                     new SourceLoc(startPos.line, startPos.pos + len));
 }
 Scanner.prototype._lexNumber = function(startPos) {
     const numStr = /\d*\.\d*|\d+/.exec(this.text.substring(this._pos))[0];
@@ -146,7 +208,7 @@ Scanner.prototype._lexNumber = function(startPos) {
     const num = parseFloat(numStr);
     this._fastForward(len);
     return new Token(num, Scanner.NUMBER, startPos,
-		     new SourceLoc(startPos.line, startPos.pos + len));
+                     new SourceLoc(startPos.line, startPos.pos + len));
 }
 Scanner.prototype.getToken = function() {
     this._skipWhitespace();
@@ -154,7 +216,7 @@ Scanner.prototype.getToken = function() {
 
     // at the end of the input, peek is null.
     if (peek === null)
-	return null;
+        return null;
 
     // keep track of the start of the token, relative to the start of
     // the current line.
@@ -164,83 +226,31 @@ Scanner.prototype.getToken = function() {
     // match two-char tokens
     switch (peek) {
     case '=':
-	if (getChar('=') === '=') {
-	    // eat the second '=', since we matched.
-	    this._fastForward(2);
-	    return new Token('==', Scanner.TOKEN, startLoc,
-			     new SourceLoc(this._line, start + 2));
-	} else {
-	    this._getChar();
-	    return new Token('=', Scanner.TOKEN, startLoc,
-			     new SourceLoc(this._line, start + 1));
-	}
-	break;
+        if (getChar('=') === '=') {
+            // eat the second '=', since we matched.
+            this._fastForward(2);
+            return new Token('==', Scanner.TOKEN, startLoc,
+                             new SourceLoc(this._line, start + 2));
+        } else {
+            this._getChar();
+            return new Token('=', Scanner.TOKEN, startLoc,
+                             new SourceLoc(this._line, start + 1));
+        }
+        break;
     default:
-	break;
+        break;
     }
 
     if (isNumberStart(peek))
-	return this._lexNumber(startLoc);
+        return this._lexNumber(startLoc);
 
     if (isIdentifierStart(peek))
-	return this._lexIdentifier(startLoc);
+        return this._lexIdentifier(startLoc);
 
     // if we haven't matched by here, it must be a simple one char
     // token.  Eat that char and return the new token object.
     this._getChar();
     return new Token(peek, Scanner.TOKEN, startLoc,
-		     new SourceLoc(this._line, start + 1));
+                     new SourceLoc(this._line, start + 1));
 }
 exports.Scanner = Scanner;
-
-/**
-   Extracts all <macro>'s into a usable format.
-*/
-parseMacros = function(macros) {
-    if (macros.length === 0)
-	return {}
-
-    // FIXME: is it parm or param?
-    paramSet = {}
-    params = macros.children('parm').map(function() {
-	const name = $(this).text();
-	paramSet[name] = true;
-	return name;
-    });
-
-
-    console.log('vars: (' + params[0] + ' ' + params[1]);
-
-    return {};
-}
-
-/**
-   Converts a string of xml data represnting an xmile model into a
-   javascript object.  The resulting javascript object can give
-   information about the model (what variables does it have, what are
-   their equations, etc), and can also create simulation objects.
-   Simulation objects can be parametarized and run (simulated),
-   generating time-series data on all of the variables.                                         
-*/
-exports.modelGen = function(xmlString) {
-    exports.err = null;
-
-    const model = {};
-    const xmile = $(xmlString);
-    if (!xmile || xmile.find('header smile').attr('version') != "1.0") {
-	exports.err = ERR_VERSION;
-	return null;
-    }
-    model.name = xmile.find('header name').text() || 'boosd model';
-
-    // get our time info: start-time, end-time, dt, etc.
-    model.ctrl = parseControlInfo(xmile.children('simspecs'));
-    if (!model.ctrl)
-	return null;
-
-    model.macros = parseMacros(xmile.children('macro'));
-    if (!model.macros)
-	return null;
-
-    return {};
-}
