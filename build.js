ec47170f6ae6eb5fc8ae3a1dba5a3b3f83cec47b
Bobby Powers <bobbypowers@gmail.com>
2013-11-21 09:55:03 -0500

    build: move javascript opts into build.js

---
+++ build.js
@@ -0,0 +1,16 @@
+({
+    baseUrl: 'lib',
+    paths: {
+        requireLib: 'vendor/require',
+        underscore: 'vendor/underscore',
+        jquery: 'vendor/jquery',
+        raphael: 'vendor/raphael',
+        snapsvg: 'vendor/snapsvg',
+        q: 'vendor/q',
+        mustache: 'vendor/mustache',
+    },
+    include: ['requireLib'],//, 'underscore', 'jquery', 'raphael', 'snapsvg', 'q', 'mustache'],
+    optimize: 'none',
+    name: 'sd',
+    out: 'build/sd.js',
+})
