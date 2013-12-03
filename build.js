8bce3f98b5a2a9ffe07023808b5ffc68494a6833
Bobby Powers <bobbypowers@gmail.com>
2013-12-03 15:16:47 -0500

    clean up build & package for browser with almond + don't require top level require

--- build.js
+++ build.js
@@ -1,16 +1,11 @@
 ({
     baseUrl: 'lib',
-    paths: {
-        requireLib: 'vendor/require',
-        underscore: 'vendor/underscore',
-        jquery: 'vendor/jquery',
-        raphael: 'vendor/raphael',
-        snapsvg: 'vendor/snapsvg',
-        q: 'vendor/q',
-        mustache: 'vendor/mustache',
+    include: ['sd'],
+    optimize: 'none',
+    name: 'vendor/almond',
+    wrap: {
+        startFile: 'lib/build/start.frag.js',
+        endFile: 'lib/build/end.frag.js'
     },
-    include: ['requireLib'],//, 'underscore', 'jquery', 'raphael', 'snapsvg', 'q', 'mustache'],
-//    optimize: 'none',
-    name: 'sd',
     out: 'build/sd.nakid.js',
 })
