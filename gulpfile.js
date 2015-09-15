f7427c73366723f8a7157604f9f1000f54992e00
Bobby Powers <bobbypowers@gmail.com>
2015-09-14 22:35:54 -0400

    build: add lib task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,29 @@ var lib = 'lib';
 var dist = '.';
 
 var rtProject = ts.createProject('tsconfig.json', { sortOutput: true });
+var libProject = ts.createProject(
+    'tsconfig.json',
+    {
+	sortOutput: true,
+	declaration: true,
+    }
+);
 
 gulp.task('runtime', function() {
     var tsRT = gulp.src('runtime/*.ts')
 	.pipe(ts(rtProject))
 	.js
 	.pipe(gulp.dest('build-rt'));
+
     return merge(tsRT, gulp.src('runtime/*.css'))
 	.pipe(buildRuntime('runtime.ts'))
 	.pipe(gulp.dest('src'));
 });
 
+gulp.task('lib', function() {
+    var tsLib = gulp.src('src/*.ts')
+	.pipe(ts(libProject));
+
+    return merge(tsLib.js, tsLib.dts)
+	.pipe(gulp.dest(lib));
+});
