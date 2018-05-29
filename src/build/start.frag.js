3e5317fc8c373a0e83c6f283a80530ec30d3b333
Bobby Powers <bobbypowers@gmail.com>
2018-05-28 20:29:22 -0400

    remove js frags

--- src/build/start.frag.js
+++
@@ -1,7 +0,0 @@
-(function (global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else {
-        global.sd = factory();
-    }
-}(typeof window !== "undefined" ? window : this, function () {
