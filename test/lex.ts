3434110f0fca409f4266a4f3c3bbae55caabe4bc
Bobby Powers <bobbypowers@gmail.com>
2018-06-08 22:46:58 -0400

    fix unit tests

--- test/lex.ts
+++ test/lex.ts
@@ -5,7 +5,7 @@
 'use strict';
 
 import * as chai from 'chai';
-import {Token, TokenType} from '../lib/type';
+import {SourceLoc, Token, TokenType} from '../lib/type';
 import {Lexer} from '../lib/lex';
 
 const expect = chai.expect;
@@ -15,206 +15,208 @@ interface LexTestData {
 	out: Token[];
 }
 
+const loc = new SourceLoc(0, 0);
+
 const LEX_TESTS: LexTestData[] = [
 	{
 		in: "a",
 		out: [
-			new Token("a", TokenType.IDENT),
+			new Token("a", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
 		in: "å",
 		out: [
-			new Token("å", TokenType.IDENT),
+			new Token("å", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
 		in: "a1_åbc________",
 		out: [
-			new Token("a1_åbc________", TokenType.IDENT),
+			new Token("a1_åbc________", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
 		in: "IF value THEN MAX(flow, 1) ELSE flow",
 		out: [
-			new Token("if", TokenType.RESERVED),
-			new Token("value", TokenType.IDENT),
-			new Token("then", TokenType.RESERVED),
-			new Token("max", TokenType.IDENT),
-			new Token("(", TokenType.TOKEN),
-			new Token("flow", TokenType.IDENT),
-			new Token(",", TokenType.TOKEN),
-			new Token("1", TokenType.NUMBER),
-			new Token(")", TokenType.TOKEN),
-			new Token("else", TokenType.RESERVED),
-			new Token("flow", TokenType.IDENT),
+			new Token("if", TokenType.RESERVED, loc, loc),
+			new Token("value", TokenType.IDENT, loc, loc),
+			new Token("then", TokenType.RESERVED, loc, loc),
+			new Token("max", TokenType.IDENT, loc, loc),
+			new Token("(", TokenType.TOKEN, loc, loc),
+			new Token("flow", TokenType.IDENT, loc, loc),
+			new Token(",", TokenType.TOKEN, loc, loc),
+			new Token("1", TokenType.NUMBER, loc, loc),
+			new Token(")", TokenType.TOKEN, loc, loc),
+			new Token("else", TokenType.RESERVED, loc, loc),
+			new Token("flow", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
 		in: "if a < 1 then 1 else 0",
 		out: [
-			new Token("if", TokenType.RESERVED),
-			new Token("a", TokenType.IDENT),
-			new Token("<", TokenType.TOKEN),
-			new Token("1", TokenType.NUMBER),
-			new Token("then", TokenType.RESERVED),
-			new Token("1", TokenType.NUMBER),
-			new Token("else", TokenType.RESERVED),
-			new Token("0", TokenType.NUMBER),
+			new Token("if", TokenType.RESERVED, loc, loc),
+			new Token("a", TokenType.IDENT, loc, loc),
+			new Token("<", TokenType.TOKEN, loc, loc),
+			new Token("1", TokenType.NUMBER, loc, loc),
+			new Token("then", TokenType.RESERVED, loc, loc),
+			new Token("1", TokenType.NUMBER, loc, loc),
+			new Token("else", TokenType.RESERVED, loc, loc),
+			new Token("0", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "IF a = b THEN 1 ELSE 0",
 		out: [
-			new Token("if", TokenType.RESERVED),
-			new Token("a", TokenType.IDENT),
-			new Token("=", TokenType.TOKEN),
-			new Token("b", TokenType.IDENT),
-			new Token("then", TokenType.RESERVED),
-			new Token("1", TokenType.NUMBER),
-			new Token("else", TokenType.RESERVED),
-			new Token("0", TokenType.NUMBER),
+			new Token("if", TokenType.RESERVED, loc, loc),
+			new Token("a", TokenType.IDENT, loc, loc),
+			new Token("=", TokenType.TOKEN, loc, loc),
+			new Token("b", TokenType.IDENT, loc, loc),
+			new Token("then", TokenType.RESERVED, loc, loc),
+			new Token("1", TokenType.NUMBER, loc, loc),
+			new Token("else", TokenType.RESERVED, loc, loc),
+			new Token("0", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "IF a >= 1 THEN b ELSE c",
 		out: [
-			new Token("if", TokenType.RESERVED),
-			new Token("a", TokenType.IDENT),
-			new Token("≥", TokenType.TOKEN),
-			new Token("1", TokenType.NUMBER),
-			new Token("then", TokenType.RESERVED),
-			new Token("b", TokenType.IDENT),
-			new Token("else", TokenType.RESERVED),
-			new Token("c", TokenType.IDENT),
+			new Token("if", TokenType.RESERVED, loc, loc),
+			new Token("a", TokenType.IDENT, loc, loc),
+			new Token("≥", TokenType.TOKEN, loc, loc),
+			new Token("1", TokenType.NUMBER, loc, loc),
+			new Token("then", TokenType.RESERVED, loc, loc),
+			new Token("b", TokenType.IDENT, loc, loc),
+			new Token("else", TokenType.RESERVED, loc, loc),
+			new Token("c", TokenType.IDENT, loc, loc),
 		]
 	},
 	// exponent 'e' is case insensitive
 	{
 		in: "5E4",
 		out: [
-			new Token("5e4", TokenType.NUMBER),
+			new Token("5e4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "5e4",
 		out: [
-			new Token("5e4", TokenType.NUMBER),
+			new Token("5e4", TokenType.NUMBER, loc, loc),
 			]
 	},
 	{
 		in: "5.0000000000000e4.00000000000000",
 		out: [
-			new Token("5.0000000000000e4.00000000000000", TokenType.NUMBER),
+			new Token("5.0000000000000e4.00000000000000", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3",
 		out: [
-			new Token("3", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3.1.1e.1.1e1e1",
 		out: [
-			new Token("3.1", TokenType.NUMBER),
-			new Token(".1e.1", TokenType.NUMBER),
-			new Token(".1e1", TokenType.NUMBER),
-			new Token("e1", TokenType.IDENT),
+			new Token("3.1", TokenType.NUMBER, loc, loc),
+			new Token(".1e.1", TokenType.NUMBER, loc, loc),
+			new Token(".1e1", TokenType.NUMBER, loc, loc),
+			new Token("e1", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
 		in: "-3.222\n",
 		out: [
-			new Token("-", TokenType.TOKEN),
-			new Token("3.222", TokenType.NUMBER),
+			new Token("-", TokenType.TOKEN, loc, loc),
+			new Token("3.222", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "-30000.222",
 		out: [
-			new Token("-", TokenType.TOKEN),
-			new Token("30000.222", TokenType.NUMBER),
+			new Token("-", TokenType.TOKEN, loc, loc),
+			new Token("30000.222", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "5.3e4.",
 		out: [
-			new Token("5.3e4.", TokenType.NUMBER),
+			new Token("5.3e4.", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 == 4 \n\n= 1",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token("==", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
-			new Token("=", TokenType.TOKEN),
-			new Token("1", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token("==", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
+			new Token("=", TokenType.TOKEN, loc, loc),
+			new Token("1", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 <> 4",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token("≠", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token("≠", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 >< 4",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token(">", TokenType.TOKEN),
-			new Token("<", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token(">", TokenType.TOKEN, loc, loc),
+			new Token("<", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 <= 4",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token("≤", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token("≤", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 AND 4",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token("&", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token("&", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 OR 4",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token("|", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token("|", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "NOT 0",
 		out: [
-			new Token("!", TokenType.TOKEN),
-			new Token("0", TokenType.NUMBER),
+			new Token("!", TokenType.TOKEN, loc, loc),
+			new Token("0", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "3 >= 4",
 		out: [
-			new Token("3", TokenType.NUMBER),
-			new Token("≥", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
+			new Token("≥", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
 		in: "hares * birth_fraction",
 		out: [
-			new Token("hares", TokenType.IDENT),
-			new Token("*", TokenType.TOKEN),
-			new Token("birth_fraction", TokenType.IDENT),
+			new Token("hares", TokenType.IDENT, loc, loc),
+			new Token("*", TokenType.TOKEN, loc, loc),
+			new Token("birth_fraction", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
@@ -232,7 +234,7 @@ const LEX_TESTS: LexTestData[] = [
 	{
 		in: "{comment before num}3",
 		out: [
-			new Token("3", TokenType.NUMBER),
+			new Token("3", TokenType.NUMBER, loc, loc),
 		]
 	},
 	{
@@ -241,44 +243,44 @@ const LEX_TESTS: LexTestData[] = [
 	{
 		in: "pulse(size_of_1_time_lynx_harvest, 4, 1e3)\n",
 		out: [
-			new Token("pulse", TokenType.IDENT),
-			new Token("(", TokenType.TOKEN),
-			new Token("size_of_1_time_lynx_harvest", TokenType.IDENT),
-			new Token(",", TokenType.TOKEN),
-			new Token("4", TokenType.NUMBER),
-			new Token(",", TokenType.TOKEN),
-			new Token("1e3", TokenType.NUMBER),
-			new Token(")", TokenType.TOKEN),
+			new Token("pulse", TokenType.IDENT, loc, loc),
+			new Token("(", TokenType.TOKEN, loc, loc),
+			new Token("size_of_1_time_lynx_harvest", TokenType.IDENT, loc, loc),
+			new Token(",", TokenType.TOKEN, loc, loc),
+			new Token("4", TokenType.NUMBER, loc, loc),
+			new Token(",", TokenType.TOKEN, loc, loc),
+			new Token("1e3", TokenType.NUMBER, loc, loc),
+			new Token(")", TokenType.TOKEN, loc, loc),
 		]
 	},
 	{
 		in: "\"hares\" * \"birth fraction\"",
 		out: [
-			new Token("\"hares\"", TokenType.IDENT),
-			new Token("*", TokenType.TOKEN),
-			new Token("\"birth fraction\"", TokenType.IDENT),
+			new Token("\"hares\"", TokenType.IDENT, loc, loc),
+			new Token("*", TokenType.TOKEN, loc, loc),
+			new Token("\"birth fraction\"", TokenType.IDENT, loc, loc),
 		]
 	},
 	{
 		in: "sales[pizza, spinach]",
 		out: [
-			new Token("sales", TokenType.IDENT),
-			new Token("[", TokenType.TOKEN),
-			new Token("pizza", TokenType.IDENT),
-			new Token(",", TokenType.TOKEN),
-			new Token("spinach", TokenType.IDENT),
-			new Token("]", TokenType.TOKEN),
+			new Token("sales", TokenType.IDENT, loc, loc),
+			new Token("[", TokenType.TOKEN, loc, loc),
+			new Token("pizza", TokenType.IDENT, loc, loc),
+			new Token(",", TokenType.TOKEN, loc, loc),
+			new Token("spinach", TokenType.IDENT, loc, loc),
+			new Token("]", TokenType.TOKEN, loc, loc),
 		]
 	},
 	{
 		in: "sales[pizza, *]",
 		out: [
-			new Token("sales", TokenType.IDENT),
-			new Token("[", TokenType.TOKEN),
-			new Token("pizza", TokenType.IDENT),
-			new Token(",", TokenType.TOKEN),
-			new Token("*", TokenType.TOKEN),
-			new Token("]", TokenType.TOKEN),
+			new Token("sales", TokenType.IDENT, loc, loc),
+			new Token("[", TokenType.TOKEN, loc, loc),
+			new Token("pizza", TokenType.IDENT, loc, loc),
+			new Token(",", TokenType.TOKEN, loc, loc),
+			new Token("*", TokenType.TOKEN, loc, loc),
+			new Token("]", TokenType.TOKEN, loc, loc),
 		]
 	}
 ];
